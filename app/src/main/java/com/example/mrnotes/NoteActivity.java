d2e45839c3396cc861e8ea0e58d1ab32118a525e
Frank Rono <frankkiplaa@gmail.com>
2020-11-06 23:01:30 +0300

    first commit

---
+++ app/src/main/java/com/example/mrnotes/NoteActivity.java
@@ -0,0 +1,87 @@
+package com.example.mrnotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_INFO = "com.example.mrnotes.NOTE_INFO ";
+    private NoteInfo mNote;
+    private boolean mIsNewNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Spinner spinnerCourses= findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses= DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses=
+                             new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
+          adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+          spinnerCourses.setAdapter(adapterCourses);
+
+      readDisplayStateValues();
+
+        EditText textNoteTitle= findViewById(R.id.text_note_title);
+        EditText textnoteText = findViewById(R.id.text_note_text);
+       if(!mIsNewNote)
+        displayNote(spinnerCourses,textNoteTitle,textnoteText);
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textnoteText) {
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+         int courseIndex = courses.indexOf(mNote.getCourse());
+        textNoteTitle.setText(mNote.getTitle());
+        textnoteText.setText(mNote.getText());
+
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        mNote = intent.getParcelableExtra(NOTE_INFO);
+        mIsNewNote = mNote==null;
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
